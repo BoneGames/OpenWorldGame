e7f2b67ed12a4c66f6d92803dcc103d907a828a7
BoneGames <benjaminjgbarnes@gmail.com>
2019-05-02 15:59:25 +1000

    navmesh chasing

---
+++ Assets/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    public Transform target;
+    public Rigidbody rigid;
+    private NavMeshAgent agent;
+    public float jump;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(target.position);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(agent.isOnNavMesh)
+        {
+            if(collision.transform.tag == "Enemy")
+            {
+                Debug.Log("jump");
+                rigid.AddForce(new Vector3(0, jump, 0), ForceMode.Impulse);
+            }
+        }
+    }
+}

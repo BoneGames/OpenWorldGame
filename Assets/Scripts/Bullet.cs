5611c1de81df12ca5f3aa22659240ba4871464ff
BoneGames <benjaminjgbarnes@gmail.com>
2019-05-09 20:26:59 +1000

    more decal stretching

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -10,12 +10,16 @@ public class Bullet : MonoBehaviour
     public float speed = 50f;
     [BoxGroup("References")] public GameObject bulletHolePrefab;
     [BoxGroup("References")] public Transform line;
+    bool hit = false;
 
     private Rigidbody rigid;
+
+    Vector3 start, end;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody>();
+        start = transform.position;
     }
     void Update()
     {
@@ -23,18 +27,38 @@ public class Bullet : MonoBehaviour
     }
     void OnCollisionEnter(Collision col)
     {
+        end = transform.position;
         // Get contact point from collision
         ContactPoint contact = col.contacts[0];
         // Spawn a BulletHole on that contact point
         GameObject bHole = Instantiate(bulletHolePrefab, contact.point, Quaternion.LookRotation(contact.normal) *
                                                      Quaternion.AngleAxis(90, Vector3.right));
-        Debug.Log("contactNormal: " + contact.normal);
-        Debug.Log("bulletTrans: " + transform.forward);
 
-      //bHole.transform.localScale = ).normalized);
+        Vector3 bulletDir = (end - start).normalized;
+        bHole.transform.rotation = Quaternion.LookRotation(bulletDir);
+
+        // the bullet is stretching in the right direction, but still needs to be oriented to be flat on the surface it lands on - is currently lying on the path the bullet took
+        //bHole.transform.right = contact.point;
+   
+            float stretch = (180 - Vector3.Angle(bulletDir, contact.normal))/72;
+            Debug.Log(stretch);
+   
+        bHole.transform.localScale = new Vector3(0.25f, 0.25f, stretch);
+
+   
+        //Debug.Log("contactNormal: " + contact.normal);
+        //Debug.Log("bulletTrans: " + transform.forward);
+        
+
+        
+        Quaternion lookRot = Quaternion.LookRotation(transform.forward, contact.normal); 
+     
+        //bHole.transform.localScale = new Vector3(0, 0, stretch);
         // Destroy self
         Destroy(gameObject);
     }
+
+  
     public void Fire(Vector3 lineOrigin, Vector3 direction)
     {
         // Set line position to origin
